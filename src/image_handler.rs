5f00a135303a78d52e94b8d4ac6eb39905b3274a
Herman Hallonqvist <herman.hallonqvist@gmail.com>
2026-01-05 18:27:19 +0100

    formatting code

--- src/image_handler.rs
+++ src/image_handler.rs
@@ -1,4 +1,4 @@
-use image::{ImageBuffer, RgbImage, Rgb};
+use image::{ImageBuffer, Rgb, RgbImage};
 
 pub struct ImageHandler {
     image: RgbImage,
@@ -6,9 +6,11 @@ pub struct ImageHandler {
 
 impl ImageHandler {
     pub fn new(width: u32, height: u32) -> Self {
-        Self { image: ImageBuffer::new(width, height) }
+        Self {
+            image: ImageBuffer::new(width, height),
+        }
     }
-    pub fn set_pixel(&mut self, x: u32, y: u32, r:u8, g:u8, b:u8) {
+    pub fn set_pixel(&mut self, x: u32, y: u32, r: u8, g: u8, b: u8) {
         self.image.put_pixel(x, y, Rgb([r, g, b]));
     }
     pub fn save(&self, path: &str) {
