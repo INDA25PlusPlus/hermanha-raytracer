07275afcbd8d70d00dbee08dd11ab076c5294b89
Herman Hallonqvist <herman.hallonqvist@gmail.com>
2026-01-05 17:49:41 +0100

    image handler:
    make new empty image with height and width, set pixels to rgb color method

---
+++ src/image_handler.rs
@@ -0,0 +1,17 @@
+use image::{ImageBuffer, RgbImage, Rgb};
+
+pub struct ImageHandler {
+    image: RgbImage,
+}
+
+impl ImageHandler {
+    pub fn new(width: u32, height: u32) -> Self {
+        Self { image: ImageBuffer::new(width, height) }
+    }
+    pub fn set_pixel(&mut self, x: u32, y: u32, r:u8, g:u8, b:u8) {
+        self.image.put_pixel(x, y, Rgb([r, g, b]));
+    }
+    pub fn save(&self, path: &str) {
+        self.image.save(path).unwrap();
+    }
+}
