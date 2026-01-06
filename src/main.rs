24c272f92fc6e03671272cb753e332526b3c0dbf
Herman Hallonqvist <herman.hallonqvist@gmail.com>
2026-01-06 17:23:53 +0100

    added infinite plane

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ mod hittable;
 mod hittable_list;
 mod image_handler;
 mod interval;
+mod plane;
 mod ray;
 mod sphere;
 mod vec3;
@@ -10,7 +11,7 @@ mod vec3;
 use camera::Camera;
 use hittable_list::HittableList;
 use image_handler::ImageHandler;
-use vec3::Point3;
+use vec3::{Point3, Vec3};
 
 fn main() {
     // Camera setup
@@ -23,8 +24,8 @@ fn main() {
 
     // World
     let mut world = HittableList::new();
+    world.add_plane(Point3::new(0.0, -0.5, 0.0), Vec3::new(0.0, 1.0, 0.0));
     world.add_sphere(Point3::new(0.0, 0.0, -1.0), 0.5);
-    world.add_sphere(Point3::new(0.0, -100.5, -1.0), 100.0);
 
     // Render
     camera.render(&world, &mut image_handler);
